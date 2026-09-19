25438a942e70bc1afe297d402a1c12652558abdf
wos <wos@axiell.com>
2026-09-20 00:50:07 +0200

    Generic kanban plan

--- boards/is-even/work/test_is_even.py
+++ boards/is-even/work/test_is_even.py
@@ -1,14 +1,17 @@
-def test_zero_is_even() -> None:
+from is_even import is_even
+
+
+def test_is_even_zero():
     assert is_even(0) is True
 
 
-def test_positive_even_is_even() -> None:
+def test_is_even_four():
     assert is_even(4) is True
 
 
-def test_positive_odd_is_odd() -> None:
+def test_is_even_seven():
     assert is_even(7) is False
 
 
-def test_negative_odd_is_odd() -> None:
+def test_is_even_negative_three():
     assert is_even(-3) is False
